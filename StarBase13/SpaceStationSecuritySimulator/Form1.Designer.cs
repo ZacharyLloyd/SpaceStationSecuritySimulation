5f9749cfa1ab3075703db0fe5414c9725290f2ea
Zachary Lloyd <zaclloyd@uat.edu>
2020-05-21 14:39:11 -0400

    New image added

--- StarBase13/SpaceStationSecuritySimulator/Form1.Designer.cs
+++ StarBase13/SpaceStationSecuritySimulator/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 10);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(439, 286);
+            this.ClientSize = new System.Drawing.Size(1162, 633);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
